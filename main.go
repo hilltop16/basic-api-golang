7cc637279652dfaf8304ee31938cf01ebf1accc8
Ben Lu <hilltop16@gmail.com>
2018-04-14 23:01:33 +0800

    created show and update methods

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "flag"
+import (
+	"flag"
+
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"log"
+	"fmt"
+)
 
 var (
 	addr = flag.String("addr",":8080", "http server address")
@@ -8,6 +15,33 @@ var (
 )
 
 func main() {
-
-
+	flag.Parse()
+	data = map[string]string{}
+	r:=httprouter.New()
+	r.GET("/entry/:key", show)
+	r.GET("/list", show)
+	r.PUT("/entry/:key/:value", update)
+	err := http.ListenAndServe(*addr, r)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
 }
+
+func show(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	k := p.ByName("key")
+	if k == "" {
+		fmt.Fprintf(w, "Read list: %v", data)
+		return
+	}
+	fmt.Fprintf(w, "Read entry: data[%s] = %s", k, data[k])
+}
+
+func update(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	k:=p.ByName("key")
+	v:=p.ByName("value")
+
+	data[k]=v
+
+	fmt.Fprintf(w, "Updated: data[%s] = %s", k, data[k])
+}
+
